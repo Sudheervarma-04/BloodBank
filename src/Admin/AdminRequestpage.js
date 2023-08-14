4545f9ce540d2d594a15c3365c82ea38b54befb3
snehil891 <snehilsah400@gmail.com>
2023-08-14 17:26:13 +0530

    admin background changes in css

--- src/Admin/AdminRequestpage.js
+++ src/Admin/AdminRequestpage.js
@@ -4,10 +4,10 @@ import '../project_scripts/dashboard'
 
 export default function AdminRequestPage(){
     return(
-        <div>
-            <h1 class="Donation_details">DONATION DETAILS</h1>
-            <p class="select">Select blood group to view details</p>
-            <div class="dropbtn">
+        <div className='admindonationbody'>
+            <h1 className="Donation_details">DONATION DETAILS</h1>
+            <p className="select">Select blood group to view details</p>
+            <div className="dropbtn">
             <select id="Blood_group_request" required>
                 <option value="">Select the blood group</option>
                 <option value="A+">A+</option>
--- src/Admin/Dashboard.js
+++ src/Admin/Dashboard.js
@@ -1,53 +1,53 @@
-import React from 'react'
-import '../project_styles/Admin-page.css'
-import '../assets/img/bloodbank logo.png'
-import '../Admin/AdminNav'
+import React from "react";
+import "../project_styles/Admin-page.css";
+import "../assets/img/bloodbank logo.png";
+import "../Admin/AdminNav";
 // import 'https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css';
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-    return(
-        <nav>
-      <div class="logo">
-        <i class="bx bx-menu menu-icon"></i>
-        <span class="logo-name">GITAM Blood Bank</span>
+  return (
+    <div className="adminbody">
+      <div className="logo">
+        <i className="bx bx-menu menu-icon"></i>
+        <span className="logo-name">GITAM Blood Bank</span>
       </div>
 
-      <div class="sidebar">
-        <div class="logo">
-          <i class="bx bx-menu menu-icon"></i>
-          <span class="logo-name">GITAM Blood Bank</span>
+      <div className="sidebar">
+        <div className="logo">
+          <i className="bx bx-menu menu-icon"></i>
+          <span className="logo-name">GITAM Blood Bank</span>
         </div>
 
-        <div class="sidebar-content">
-          <ul class="lists">
-            <li class="list">
-              <Link to="/admin" class="nav-link">
-                <i class='bx bxs-dashboard dash-board'></i>
-                <span class="link">Dashboard</span>
+        <div className="sidebar-content">
+          <ul className="lists">
+            <li className="list">
+              <Link to="/admin" className="nav-link">
+                <i className="bx bxs-dashboard dash-board"></i>
+                <span className="link">Dashboard</span>
               </Link>
             </li>
-            <li class="list">
-              <Link to="/admindonatepage" class="nav-link">
-                <i class='bx bxs-donate-blood donate-blood'></i>
-                <span class="link">Donations</span>
+            <li className="list">
+              <Link to="/admindonatepage" className="nav-link">
+                <i className="bx bxs-donate-blood donate-blood"></i>
+                <span className="link">Donations</span>
               </Link>
             </li>
-            <li class="list">
-              <Link to="/adminrequestpage" class="nav-link">
-                <i class='bx bx-donate-blood donate-blood'></i>
-                <span class="link">Requests</span>
+            <li className="list">
+              <Link to="/adminrequestpage" className="nav-link">
+                <i className="bx bx-donate-blood donate-blood"></i>
+                <span className="link">Requests</span>
               </Link>
             </li>
-            <li class="list">
-              <a href="../Login.html" class="nav-link">
-                <i class='bx bx-log-out log-out'></i>
-                <span class="link">Log-out</span>
+            <li className="list">
+              <a href="../Login.html" className="nav-link">
+                <i className="bx bx-log-out log-out"></i>
+                <span className="link">Log-out</span>
               </a>
             </li>
           </ul>
         </div>
       </div>
-    </nav>
-    )  
+    </div>
+  );
 }
